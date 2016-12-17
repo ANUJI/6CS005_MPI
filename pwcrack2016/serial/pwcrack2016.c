33e48c236249d6910b83e29960d7f7dc2f8586f1
anuji <anuji.de.silva@gmail.com>
2016-12-17 22:32:05 +0530

    Looking for possible password combinations
    Looking for possible password combinations

--- pwcrack2016/serial/pwcrack2016.c
+++ pwcrack2016/serial/pwcrack2016.c
@@ -28,6 +28,7 @@ void decrypt_password(int, char*, char**);
 
 //decrypting the password
 void decrypt_password(const int password_length, char* password, char** plain) {
+  //Looking for possible password combinations
     int possibilties = pow(ALPHABET_SIZE, password_length);
     char candidates[possibilties][password_length + 1];
     long i = 0, val = 0;
